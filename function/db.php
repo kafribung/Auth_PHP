f6d2a682b8638848bb27906a8cba30f71a590fc8
kafri bung <kafribung07@gmail.com>
2020-04-27 23:39:33 +0800

    first

---
+++ function/db.php
@@ -0,0 +1,8 @@
+<?php 
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db   = "login_register_php";
+
+$link = mysqli_connect($host, $user, $pass, $db) or die("Bung DB mu error " . mysqli_connect_error());
